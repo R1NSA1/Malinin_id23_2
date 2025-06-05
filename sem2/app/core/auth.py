ef165ea54c758474bca3cc82134338dd9e5aecb9
Ilya Malinin <147249192+R1NSA1@users.noreply.github.com>
2025-06-05 12:47:21 +0300

    Add files via upload

---
+++ sem2/app/core/auth.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from fastapi import Depends, HTTPException, status, Header
+from sqlalchemy.orm import Session
+from app.cruds.user import get_user_by_id  # изменили функцию поиска
+from app.core.config import settings
+from app.db.database import get_db
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(authorization: str = Header(None), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось проверить учетные данные",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    if not authorization or not authorization.startswith("Bearer "):
+        raise credentials_exception
+    token = authorization[len("Bearer "):]
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user_by_id(db, int(user_id))
+    if user is None:
+        raise credentials_exception
+    return user
+
+
